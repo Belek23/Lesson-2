cb5963003a561188060ff52f1fa68980f556b727
Belek Abdy <beshik2013@yandex.ru>
2024-07-09 12:51:51 +0300

    add Lesson3:Task3

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// output: zero even elements: 1 0 3 0 5
+
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        System.Console.Write($"{e} ");
+    }
+}
+
+int[] array = { -2, -1, 566, 33333 };
+ZeroEvenElements(array);
+PrintArray(array); // Output: 1 0 3 0 5
