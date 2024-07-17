7a69e1bc80c8d27b36520fd5b3eb74285bc15f0d
Belek Abdy <beshik2013@yandex.ru>
2024-07-17 13:33:41 +0300

    add Lesson5:Task1

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// RECURSION. Factorial
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        System.Console.WriteLine($"Stop recursion: {n}");
+        return 1;
+    }
+    System.Console.WriteLine(n);
+    return n * Fact(n - 1); // return !n * (n-1)!
+}
+
+System.Console.Write(Fact(5));
+
